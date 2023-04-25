d308fce8d8ba707fec0044bb8910e39c8b9db6ed
Dananau Stefan <95748416+TR45C0X@users.noreply.github.com>
2023-04-25 20:32:59 +0300

    Initial Commit

---
+++ DataLayer/Entities/Grade.cs
@@ -0,0 +1,12 @@
+﻿using DataLayer.Enums;
+
+namespace DataLayer.Entities;
+
+public class Grade : BaseEntity
+{
+    public double Value { get; set; }
+    public CourseType Course { get; set; }
+
+    public int StudentId { get; set; }
+    public Student Student { get; set; }
+}
---
+++ Project/Settings/Dependencies.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer.Repositories;
+using Core.Services;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Settings
+{
+    public static class Dependencies
+    {
+
+        public static void Inject(WebApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.Services.AddControllers();
+            applicationBuilder.Services.AddSwaggerGen();
+
+            applicationBuilder.Services.AddDbContext<AppDbContext>();
+
+            AddRepositories(applicationBuilder.Services);
+            AddServices(applicationBuilder.Services);
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+            services.AddScoped<StudentService>();
+            services.AddScoped<ClassService>();
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<StudentsRepository>();
+            services.AddScoped<ClassRepository>();
+            services.AddScoped<UnitOfWork>();
+        }
+
+    }
+}
